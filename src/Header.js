7289a1c4686143c7ebe462a4eea28289bbd5be23
asbren13 <asbren13@gmail.com>
2021-02-25 19:52:07 -0500

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+export default function Header() {
+	return (
+		<header className="header-section">
+			<div className="name-section">
+			 	<div className="header-name">alison van de berghe</div>
+			 </div>
+			 <div className="tabs-section">
+				<i className="fas fa-wine-glass"/>
+			 </div>
+		</header>
+	);	
+};
